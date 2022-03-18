e59c16e78d153c2bf75234270ff056bb1a80b7b0
kasyoki.justus <justuskasyoki57@gmail.com>
2022-03-18 15:52:27 +0300

    commit

--- Models/CreateAdvanceRequest.cs
+++ Models/CreateAdvanceRequest.cs
@@ -7,18 +7,18 @@ namespace LMS.Models
 {
     public class CreateAdvanceRequest
     {
-        public string No { get; set; }
-        public string Item { get; set; }
-        public string ItemDescription { get; set; }
-        public string UnitOfMeasure { get; set; }
+        public string DateOfRequest { get; set; }
+        public string DateDue { get; set; }
+        public string Balance { get; set; }
+        public string MissionSummary { get; set; }
         public string NoOfUnits { get; set; }
-        public string UnitCost { get; set; }
-        public string Currency { get; set; }
+        public string Amount { get; set; }
+        public string ExchangeRate { get; set; }
         public string Purpose { get; set; }
-        public string AttachmentName { get; set; }
-        public string AttachmentId { get; set; }
-        public string AdvancedAmount { get; set; }
-        public string AdvancedAmountLCY { get; set; }
+        public string AdvanceRequestLineNo { get; set; }
+        public string BudgetLineCode { get; set; }
+        public string UnitCost { get; set; }
+        public string ItemDescription { get; set; }
         public string ClaimedAmount { get; set; }
         public string ClaimedAmountLCY { get; set; }
         public string SurrenderedAmount { get; set; }

aa465259cb9e342020640cdbeb6bd604565b968e
Joseph K Justus <justuskasyoki57@gmail.com>
2022-03-21 12:29:32 +0300

    commit

--- Controllers/AdvanceRequestController.cs
+++ Controllers/AdvanceRequestController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using LMS.CustomsClasses;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +12,9 @@ namespace LMS.Controllers
 {
     public class AdvanceRequestController : Controller
     {
+        static string folderPath = System.Web.HttpContext.Current.Server.MapPath("~/Uploads/");
+        static string fileforDownload = "";
+        static string attachmentName = "";
         // GET: AdvanceRequest
         public ActionResult Index()
         {
@@ -15,7 +22,86 @@ namespace LMS.Controllers
         }
         public ActionResult AdvanceRequest()
         {
+            System.Web.HttpContext.Current.Session["IsAdvanceActive"] = "active";
+            System.Web.HttpContext.Current.Session["IsDashboardActive"] = "";
+            System.Web.HttpContext.Current.Session["IsClaimActive"] = "";
+            System.Web.HttpContext.Current.Session["IsSurrenderActive"] = "";
+            System.Web.HttpContext.Current.Session["IsAppriasalActive"] = "";
+            System.Web.HttpContext.Current.Session["IsApprovalEntriesActive"] = "";
+            System.Web.HttpContext.Current.Session["IsLeavesActive"] = "";
+            System.Web.HttpContext.Current.Session["IsRecallActive"] = "";
+            System.Web.HttpContext.Current.Session["IsReportsActive"] = "";
+            System.Web.HttpContext.Current.Session["IsTrainingActive"] = "";
+            System.Web.HttpContext.Current.Session["IsProfileActive"] = "";
+            System.Web.HttpContext.Current.Session["IsTransportRequestActive"] = "";
+
+            if (Session["Logged"].Equals("No"))
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else if (Session["Logged"].Equals("Yes"))
+            {
+                if (Session["RequirePasswordChange"].Equals("TRUE"))
+                {
+                    Response.Redirect("OneTimePass.aspx");
+                }
+            }
             return View();
+        }
+        protected void btn_download_Click(object sender, EventArgs e)
+        {
+            Response.ContentType = "Application/pdf";
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + attachmentName + "");
+            // Response.TransmitFile(Server.MapPath("~/doc/help.pdf"));
+            Response.TransmitFile(fileforDownload);
+            Response.End();
+        }
+        protected void View_Click(object sender, EventArgs e)
+        {
+            string pdfPath = fileforDownload;
+            WebClient client = new WebClient();
+            Byte[] buffer = client.DownloadData(pdfPath);
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-length", buffer.Length.ToString());
+            Response.BinaryWrite(buffer);
+        }
+      
+        public static string GenerateAdvanceRequest()
+        {
+            string _Status = "900";
+            string _Message = "";
+            string username = System.Web.HttpContext.Current.Session["Username"].ToString();
+
+
+            string pdfName = "StaffAdvance" + username + ".pdf";
+            string FileName = folderPath + pdfName;
+
+            try
+            {
+                string XMLRequestResponse = WebserviceConfig.ObjNav.ExportStaffAdvance(username, FileName);
+
+                attachmentName = pdfName;
+
+                fileforDownload = FileName;
+
+                dynamic json = JObject.Parse(XMLRequestResponse);
+
+                _Status = json.Status;
+                _Message = json.Msg;
+            }
+            catch (Exception es)
+            {
+                AppFunctions.WriteLog(es.Message);
+            }
+
+
+            var _RequestResponse = new RequestResponse
+            {
+                Status = _Status,
+                Message = _Message
+            };
+
+            return JsonConvert.SerializeObject(_RequestResponse);
         }
     }
 }
--- Controllers/AdvanceRequestsController.cs
+++ Controllers/AdvanceRequestsController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using LMS.CustomsClasses;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,7 +20,263 @@ namespace LMS.Controllers
         }
         public ActionResult AdvanceRequests()
         {
+            System.Web.HttpContext.Current.Session["IsAdvanceActive"] = "active";
+            System.Web.HttpContext.Current.Session["IsDashboardActive"] = "";
+            System.Web.HttpContext.Current.Session["IsClaimActive"] = "";
+            System.Web.HttpContext.Current.Session["IsSurrenderActive"] = "";
+            System.Web.HttpContext.Current.Session["IsAppriasalActive"] = "";
+            System.Web.HttpContext.Current.Session["IsApprovalEntriesActive"] = "";
+            System.Web.HttpContext.Current.Session["IsLeavesActive"] = "";
+            System.Web.HttpContext.Current.Session["IsRecallActive"] = "";
+            System.Web.HttpContext.Current.Session["IsReportsActive"] = "";
+            System.Web.HttpContext.Current.Session["IsTrainingActive"] = "";
+            System.Web.HttpContext.Current.Session["IsProfileActive"] = "";
+            System.Web.HttpContext.Current.Session["IsTransportRequestActive"] = "";
+
+            Session["ErrorMessage"] = "";
+
+            if (Session["Logged"].Equals("No"))//set to No
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else if (Session["Logged"].Equals("Yes"))
+            {
+                if (Session["RequirePasswordChange"].Equals("TRUE"))
+                {
+                    Response.Redirect("OneTimePass.aspx");
+                }
+                else
+                {
+                    string status = Request.QueryString["status"].Trim();
+
+
+                    if (status == "")
+                    {
+                        Response.Redirect(Request.UrlReferrer.ToString());
+                    }
+                    else
+                    {
+                        LoadTable(status);
+                    }
+                }
+            }
             return View();
+        }
+        private void LoadTable(string status)
+        {
+
+            string username = System.Web.HttpContext.Current.Session["Username"].ToString();
+
+            DataTable dt = new DataTable();
+
+
+            dt.Clear();
+
+            dt = AdvanceRequestsXMLRequests.GetAdvanceRequestsList(status, username);
+
+            // DataTable 
+            //Building an HTML string.
+            StringBuilder html = new StringBuilder();
+            //Table start.
+            html.Append("<table class='table table-bordered' id='dataTable' width='100%' cellspacing='0'>");
+            //Building the Header row.
+            html.Append("<thead>");
+            html.Append("<tr>");
+            foreach (DataColumn column in dt.Columns)
+            {
+                html.Append("<th>");
+                html.Append(column.ColumnName);
+                html.Append("</th>");
+            }
+            html.Append("</tr>");
+            html.Append("</thead>");
+
+            html.Append("<tfoot>");
+            html.Append("<tr>");
+            foreach (DataColumn column in dt.Columns)
+            {
+                html.Append("<th>");
+                html.Append(column.ColumnName);
+                html.Append("</th>");
+            }
+            html.Append("</tr>");
+            html.Append("</tfoot>");
+
+            //Building the Data rows.
+            html.Append("<tbody>");
+            foreach (DataRow row in dt.Rows)
+            {
+                html.Append("<tr>");
+                foreach (DataColumn column in dt.Columns)
+                {
+                    html.Append("<td>");
+                    html.Append(row[column.ColumnName]);
+                    html.Append("</td>");
+                }
+                html.Append("</tr>");
+            }
+            html.Append("</tbody>");
+            //Table end.
+            html.Append("</table>");
+            string strText = html.ToString();
+            ////Append the HTML string to Placeholder.
+            // placeholder.Controls.Add(new Literal { Text = html.ToString() });
+        }
+
+        public static string DeleteRecord(string param1)
+        {
+            string AdvanceRequestHdrNo = AppFunctions.Base64Decode(param1);
+            string status = "";
+            string Msg = "";
+
+            try
+            {
+                string DeleteAdvanceRequestXMLResponse = AdvanceRequestsXMLRequests.DeleteAdvanceRequest(AdvanceRequestHdrNo);
+
+                dynamic json = JObject.Parse(DeleteAdvanceRequestXMLResponse);
+
+                status = json.Status;
+                Msg = json.Msg;
+
+            }
+            catch (Exception e)
+            {
+                Msg = e.Message;
+            }
+
+            var _RequestResponse = new RequestResponse
+            {
+                Status = status,
+                Message = Msg
+            };
+
+            return JsonConvert.SerializeObject(_RequestResponse);
+        }
+        
+        public static string SubmitRecord(string param1)
+        {
+            string AdvanceRequestHdrNo = AppFunctions.Base64Decode(param1);
+            string status = "";
+            string Msg = "";
+
+            try
+            {
+                string SubmitAdvanceRequestXMLResponse = AdvanceRequestsXMLRequests.SubmitAdvanceRequest(AdvanceRequestHdrNo);
+
+                dynamic json = JObject.Parse(SubmitAdvanceRequestXMLResponse);
+
+                status = json.Status;
+                Msg = json.Msg;
+
+            }
+            catch (Exception e)
+            {
+                Msg = e.Message;
+            }
+
+            var _RequestResponse = new RequestResponse
+            {
+                Status = status,
+                Message = Msg
+            };
+
+            return JsonConvert.SerializeObject(_RequestResponse);
+        }
+       
+        public static string CancelRequest(string param1)
+        {
+            string AdvanceRequestHdrNo = AppFunctions.Base64Decode(param1);
+            string status = "";
+            string Msg = "";
+
+            try
+            {
+                string DeleteAdvanceRequestXMLResponse = AdvanceRequestsXMLRequests.CancelAdvanceRequest(AdvanceRequestHdrNo, "0");
+
+                dynamic json = JObject.Parse(DeleteAdvanceRequestXMLResponse);
+
+                status = json.Status;
+                Msg = json.Msg;
+
+            }
+            catch (Exception e)
+            {
+                Msg = e.Message;
+            }
+
+            var _RequestResponse = new RequestResponse
+            {
+                Status = status,
+                Message = Msg
+            };
+
+            return JsonConvert.SerializeObject(_RequestResponse);
+        }
+        
+        public static string DelegateRequest(string param1)
+        {
+            string EntryNo = AppFunctions.Base64Decode(param1);
+            string status = "";
+            string Msg = "";
+
+            try
+            {
+                string SubmitAdvanceRequestXMLResponse = WebserviceConfig.ObjNav.DelegateWorkflowApprovalRequest(Convert.ToInt32(EntryNo));
+
+                dynamic json = JObject.Parse(SubmitAdvanceRequestXMLResponse);
+
+                status = json.Status;
+                Msg = json.Msg;
+
+            }
+            catch (Exception e)
+            {
+                Msg = e.Message;
+            }
+
+            var _RequestResponse = new RequestResponse
+            {
+                Status = status,
+                Message = Msg
+            };
+
+            return JsonConvert.SerializeObject(_RequestResponse);
+        }
+       
+        public static string PrintRequest(string param1)
+        {
+            string AdvanceRequestHdrNo = param1;
+
+            string status = "000";
+            string Msg = "Success";
+
+            string folderPath = System.Web.HttpContext.Current.Server.MapPath("~/Uploads/");
+
+            string ExportToPath = folderPath + AdvanceRequestHdrNo + ".pdf";
+
+            try
+            {
+                string SubmitAdvanceRequestXMLResponse = WebserviceConfig.ObjNav.ExportAdvanceRequestReport(0, AdvanceRequestHdrNo, ExportToPath);
+
+                dynamic json = JObject.Parse(SubmitAdvanceRequestXMLResponse);
+
+                status = json.Status;
+                Msg = json.Msg;
+
+            }
+            catch (Exception e)
+            {
+                Msg = e.Message;
+                AppFunctions.WriteLog(e.Message);
+            }
+
+            var _RequestResponse = new RequestResponse
+            {
+                Status = status,
+                Message = Msg
+            };
+
+            return JsonConvert.SerializeObject(_RequestResponse);
         }
     }
 }

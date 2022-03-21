fe3d3a593b93ab2c539765dee5c954c802da91ab
Joseph K Justus <justuskasyoki57@gmail.com>
2022-03-21 12:14:53 +0300

    commit

---
+++ Controllers/AdvanceRequestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS.Controllers
+{
+    public class AdvanceRequestController : Controller
+    {
+        // GET: AdvanceRequest
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AdvanceRequest()
+        {
+            return View();
+        }
+    }
+}

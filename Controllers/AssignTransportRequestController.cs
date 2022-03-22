0e74e29500098ab007ee93a97948355158ead730
kasyoki.justus <justuskasyoki57@gmail.com>
2022-03-22 16:35:49 +0300

    commit

---
+++ Controllers/AssignTransportRequestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS.Controllers
+{
+    public class AssignTransportRequestController : Controller
+    {
+        // GET: AssignTransportRequest
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AssignTransportRequest()
+        {
+            return View();
+        }
+    }
+}

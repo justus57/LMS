5b44ba61b48f2c21f7c7fff371873a760405dd9d
kasyoki.justus <justuskasyoki57@gmail.com>
2022-03-28 17:33:22 +0300

    commit

---
+++ Models/EditPML.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS.Models
+{
+    public class EditPML
+    {
+        public string PMLCode { get; set; }
+        public string Description { get; set; }
+    }
+}

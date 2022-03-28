a857188dc9572bdbd1b2c01e39c8295213b6d41f
kasyoki.justus <justuskasyoki57@gmail.com>
2022-03-28 17:01:07 +0300

    commit

---
+++ Controllers/EditPMLController.cs
@@ -0,0 +1,91 @@
+﻿using LMS.CustomsClasses;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS.Controllers
+{
+    public class EditPMLController : Controller
+    {
+        static string _PMLNumber = "";
+        // GET: EditPML
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EditPML()
+        {
+            System.Web.HttpContext.Current.Session["IsAdvanceActive"] = "";
+            System.Web.HttpContext.Current.Session["IsDashboardActive"] = "";
+            System.Web.HttpContext.Current.Session["IsClaimActive"] = "";
+            System.Web.HttpContext.Current.Session["IsSurrenderActive"] = "";
+            System.Web.HttpContext.Current.Session["IsAppriasalActive"] = "active";
+            System.Web.HttpContext.Current.Session["IsApprovalEntriesActive"] = "";
+            System.Web.HttpContext.Current.Session["IsLeavesActive"] = "";
+            System.Web.HttpContext.Current.Session["IsRecallActive"] = "";
+            System.Web.HttpContext.Current.Session["IsReportsActive"] = "";
+            System.Web.HttpContext.Current.Session["IsTrainingActive"] = "";
+            System.Web.HttpContext.Current.Session["IsProfileActive"] = "";
+            System.Web.HttpContext.Current.Session["IsTransportRequestActive"] = "";
+
+            if (Session["Logged"].Equals("No"))
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else if (Session["Logged"].Equals("Yes"))
+            {
+                if (Session["RequirePasswordChange"].Equals("TRUE"))
+                {
+                    Response.Redirect("OneTimePass.aspx");
+                }
+                else
+                {
+                    string i = Request.QueryString["id"].Trim();
+
+                    if (i == "")
+                    {
+                        Response.Redirect(Request.UrlReferrer.ToString());
+                    }
+                    else
+                    {
+                        _PMLNumber = AppFunctions.Base64Decode(i);
+                    }
+                }
+            }
+            return View();
+        }
+        public static string LoadPML()
+        {
+            string GetAppraisalPMLDetailsResponse = DefineAppraisalSectionsXMLRequests.GetAppraisalPMLDetails(_PMLNumber);
+            return GetAppraisalPMLDetailsResponse;
+        }
+        public static string UpdatePML(string param1, string param2)
+        {
+            string status = "";
+            string response = "";
+            string PMLCode = param1;
+            string description = param2;
+            string xmlresponse = DefineAppraisalSectionsXMLRequests.CreatePML(PMLCode, description, _PMLNumber);
+
+            dynamic json = JObject.Parse(xmlresponse);
+            response = json.Msg;
+            status = json.Status;
+
+            if (status == "000")
+            {
+                response = "The level of performance was successfully updated";
+            }
+
+            var _RequestResponse = new RequestResponse
+            {
+                Message = response,
+                Status = status
+            };
+            return JsonConvert.SerializeObject(_RequestResponse);
+        }
+    }
+}

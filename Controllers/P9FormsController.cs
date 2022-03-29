ccd34fca1e49b15d660533d8acf634b32cebf11f
kasyoki.justus <justuskasyoki57@gmail.com>
2022-03-29 12:39:21 +0300

    commit

---
+++ Controllers/P9FormsController.cs
@@ -0,0 +1,124 @@
+﻿using LMS.CustomsClasses;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace LMS.Controllers
+{
+    public class P9FormsController : Controller
+    {
+        static string folderPath = System.Web.HttpContext.Current.Server.MapPath("~/Uploads/");
+        static string fileforDownload = "";
+        static string attachmentName = "";
+    
+
+        // GET: P9Forms
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult P9Forms()
+        {
+            System.Web.HttpContext.Current.Session["IsAdvanceActive"] = "";
+            System.Web.HttpContext.Current.Session["IsDashboardActive"] = "";
+            System.Web.HttpContext.Current.Session["IsClaimActive"] = "";
+            System.Web.HttpContext.Current.Session["IsSurrenderActive"] = "";
+            System.Web.HttpContext.Current.Session["IsAppriasalActive"] = "";
+            System.Web.HttpContext.Current.Session["IsApprovalEntriesActive"] = "";
+            System.Web.HttpContext.Current.Session["IsLeavesActive"] = "";
+            System.Web.HttpContext.Current.Session["IsRecallActive"] = "";
+            System.Web.HttpContext.Current.Session["IsReportsActive"] = "active";
+            System.Web.HttpContext.Current.Session["IsTrainingActive"] = "";
+            System.Web.HttpContext.Current.Session["IsProfileActive"] = "";
+            System.Web.HttpContext.Current.Session["IsTransportRequestActive"] = "";
+
+            if (Session["Logged"].Equals("No"))
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else if (Session["Logged"].Equals("Yes"))
+            {
+                if (Session["RequirePasswordChange"].Equals("TRUE"))
+                {
+                    Response.Redirect("OneTimePass.aspx");
+                }
+                else
+                {
+                    CreateYearsList();
+                }
+            }
+            return View();
+        }
+
+        private void CreateYearsList()
+        {
+            
+            //create Loop to Insert years in descending order
+
+            int CurrentYear = DateTime.Now.Year;
+
+            for (int year = (CurrentYear - 5); year <= CurrentYear; year++)
+            {
+                //P9Year.Items.Insert(0, new ListItem(year.ToString(), year.ToString()));
+            }
+            //P9Year.Items.Insert(0, new ListItem(" ", ""));
+        }
+
+        protected void btn_download_Click(object sender, EventArgs e)
+        {
+            Response.ContentType = "Application/pdf";
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + attachmentName + "");
+            // Response.TransmitFile(Server.MapPath("~/doc/help.pdf"));
+            Response.TransmitFile(fileforDownload);
+            Response.End();
+        }
+        protected void View_Click(object sender, EventArgs e)
+        {
+            string pdfPath = fileforDownload;
+            WebClient client = new WebClient();
+            Byte[] buffer = client.DownloadData(pdfPath);
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-length", buffer.Length.ToString());
+            Response.BinaryWrite(buffer);
+        }
+        
+        public static string GenerateP9Form(string param1)
+        {
+            string _Status = "900";
+            string _Message = "An error occured";
+            string username = System.Web.HttpContext.Current.Session["Username"].ToString();
+
+            string Year = param1;
+            //
+            string pdfName = "P9form_" + username + "_" + Year + ".pdf";
+            string FileName = folderPath + pdfName;
+
+            //delete Old Files
+
+            string XMLRequestResponse = GenerateReports.GenerateP9Form(username, FileName, Year);
+
+            attachmentName = pdfName;
+
+            fileforDownload = FileName;
+
+            dynamic json = JObject.Parse(XMLRequestResponse);
+
+            _Status = json.Status;
+            _Message = json.Msg;
+
+            var _RequestResponse = new RequestResponse
+            {
+                Status = _Status,
+                Message = _Message
+            };
+
+            return JsonConvert.SerializeObject(_RequestResponse);
+        }
+    }
+}

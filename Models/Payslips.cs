3760cf1b03e9bb22f11abd5a1daeb61ee454a024
kasyoki.justus <justuskasyoki57@gmail.com>
2022-03-29 13:40:45 +0300

    commit

---
+++ Models/Payslips.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS.Models
+{
+    public class Payslips
+    {
+      public string PayslipMonth { get; set; }
+    }
+}

dc930ab14620d2c0405c593406833803e87a0b2b
kasyoki.justus <justuskasyoki57@gmail.com>
2022-04-01 11:12:39 +0300

    commit

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -130,7 +130,7 @@ namespace LMS.Controllers
                     System.Web.HttpContext.Current.Session["AdvanceRequests"] = "True" ;
                     System.Web.HttpContext.Current.Session["IsAdvanceActive"] = "";
                     System.Web.HttpContext.Current.Session["IsDashboardActive"] = "";
-                    System.Web.HttpContext.Current.Session["IsClaimActive"] = "";
+                    System.Web.HttpContext.Current.Session["IsClaimActive"] = "True";
                     System.Web.HttpContext.Current.Session["IsSurrenderActive"] = "";
                     System.Web.HttpContext.Current.Session["IsAppriasalActive"] = "TRUE";
                     System.Web.HttpContext.Current.Session["IsApprovalEntriesActive"] = "active";

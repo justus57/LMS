aa6e5d9072d9caca4048f8763fa92ad55d1a53c1
kasyoki.justus <justuskasyoki57@gmail.com>
2022-07-22 10:32:22 +0300

    commit

---
+++ OshoPortal-master/OshoPortal-master/Controllers/EditRequisitionController.cs
@@ -0,0 +1,48 @@
+﻿using OshoPortal.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OshoPortal.Controllers
+{
+    public class EditRequisitionController : Controller
+    {
+        // GET: EditRequisition
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EditRequisition()
+        {
+            var log = System.Web.HttpContext.Current.Session["logged"] = "yes";
+            var passRequired = System.Web.HttpContext.Current.Session["RequirePasswordChange"] = true || false;
+            if ((string)log == "No")
+            {
+                Response.Redirect("/Account/login");
+            }
+            else if ((string)log == "yes")
+            {
+                if ((string)passRequired == "true")
+                {
+                    Response.Redirect("/Account/OneTimePassword");
+                }
+                else
+                {
+                    string s = Request.QueryString["id"].Trim();
+                    if (s == "")
+                    {
+                        Response.Redirect(Request.UrlReferrer.ToString());
+                    }
+                    else
+                    {
+                        string Requisition  = Functions.Base64Decode(s);
+                        ViewBag.WordHtml = Requisition;
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ OshoPortal-master/OshoPortal-master/Models/EditRequisition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OshoPortal.Models
+{
+    public class EditRequisition
+    {
+            public string Date { get; set; }
+            public string Description { get; set; }
+            public string cost { get; set; }
+            public string UnitOfMeasure { get; set; }
+            public string NOofItems { get; set; }
+            public string Comment { get; set; }
+            public string SelectDate { get; set; }
+    }
+}
